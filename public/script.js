8850ab33c03526d574744c4c6ff855d62b5310bd
lemokh <byndasma@gmail.com>
2021-02-07 13:08:40 +0400

    final commit

--- public/script.js
+++ public/script.js
@@ -1,65 +1,123 @@
+var PRICE = 9.99;
+var LOAD_NUM = 10;
+
+var pusher = new Pusher("[PUSHER_KEY]", {
+  cluster: "[PUSHER_CLUSTER]",
+  encrypted: true
+});
+
 new Vue({
   el: "#app",
   data: {
     total: 0,
-    products: [],
+    items: [],
+    results: [],
     cart: [],
-    search: "cat",
-    lastSearch: ""
+    newSearch: "anime",
+    lastSearch: "",
+    loading: false,
+    price: PRICE,
+    pusherUpdate: false
   },
-  methods: {
-    addToCart: function(product) {
-      this.total += product.price;
-      let found = false;
-      for (let i = 0; i < this.cart.length; i++) {
-        if (this.cart[i].id) {
-          this.cart[i].qty++;
-          found = true;
-        }
+  mounted: function() {
+    this.onSubmit();
+    var elem = document.getElementById("product-list-bottom");
+    var watcher = scrollMonitor.create(elem);
+    var vue = this;
+    watcher.enterViewport(function() {
+      vue.appendItems();
+    });
+    var channel = pusher.subscribe("cart");
+    channel.bind("update", function(data) {
+      vue.pusherUpdate = true;
+      vue.cart = data;
+      vue.total = 0;
+      for (var i = 0; i < vue.cart.length; i++) {
+        vue.total += PRICE * vue.cart[i].qty;
       }
-      if (!found) {
-        this.cart.push({
-          id: product.id,
-          title: product.title,
-          price: product.price,
-          qty: 1
-        });
-      }
-    },
-    inc: function(item) {
-      // console.log("inc");
-      item.qty++;
-      this.total += item.price;
-    },
-    dec: function(item) {
-      // console.log("dec");
-      item.qty--;
-      this.total -= item.price;
-      if (item.qty <= 0) {
-        let i = this.cart.indexOf(item);
-        this.cart.splice(i, 1);
-      }
-    },
-    onSubmit: function() {
-      // vue-response
-      // console.log("Search");
-      this.products = [];
-      this.loading = true;
-      let path = "/search?q=".concat(this.search);
-      this.$http.get(path).then(function(response) {
-        // console.log(response);
-        this.products = response.body;
-        this.lastSearch = this.search;
-        this.loading = false;
-      });
-    }
+    });
   },
   filters: {
     currency: function(price) {
       return "$".concat(price.toFixed(2));
     }
   },
-  created: function() {
-    this.onSubmit();
+  computed: {
+    noMoreItems: function() {
+      return (
+        this.results.length === this.items.length && this.results.length > 0
+      );
+    }
+  },
+  watch: {
+    cart: {
+      handler: function(val) {
+        if (!this.pusherUpdate) {
+          this.$http.post("/cart_update", val);
+        } else {
+          this.pusherUpdate = false;
+        }
+      },
+      deep: true
+    }
+  },
+  methods: {
+    appendItems: function() {
+      if (this.items.length < this.results.length) {
+        var append = this.results.slice(
+          this.items.length,
+          this.items.length + LOAD_NUM
+        );
+        this.items = this.items.concat(append);
+      }
+    },
+    onSubmit: function() {
+      if (this.newSearch.length) {
+        this.lastSearch = this.newSearch;
+        this.loading = true;
+        this.items = [];
+        this.results = [];
+        this.$http.get("/search/".concat(this.newSearch)).then(function(res) {
+          this.loading = false;
+          this.results = res.data;
+          this.appendItems();
+        });
+      }
+    },
+    addItem: function(index) {
+      var item = this.items[index];
+      var found = false;
+      for (var i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          this.cart[i].qty++;
+          found = true;
+        }
+      }
+      if (!found) {
+        this.cart = this.cart.concat({
+          id: item.id,
+          title: item.title,
+          qty: 1,
+          price: PRICE
+        });
+      }
+      this.total += PRICE;
+    },
+    inc: function(item) {
+      item.qty++;
+      this.total += PRICE;
+    },
+    dec: function(item) {
+      item.qty--;
+      this.total -= PRICE;
+      if (item.qty <= 0) {
+        for (var i = 0; i < this.cart.length; i++) {
+          if (this.cart[i].id === item.id) {
+            this.cart.splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
   }
 });
